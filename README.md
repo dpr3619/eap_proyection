21978a6668ad57861500a24173130f0a99c7e1fe
Daniel <dpr3619@gmail.com>
2025-04-26 10:14:52 -0500

    Adding updates to readem. Finished the geih extraction for informal data

--- README.md
+++ README.md
@@ -3,8 +3,13 @@ The main objective of this repository is to document the process and the code de
 
 ## ETL
 
-We use the GEIH (Gran encuesta integrada de hogares) data that contains all the information about the EAP in Colombia, the data has monthly data about the EAP population and information about employment in each one of the major economic sectors.
+We use the GEIH (Gran encuesta integrada de hogares) data that contains all the information about the EAP in Colombia, the data has monthly data about the EAP population and information about employment in each one of the major economic sectors. We also use the GEIHISS data that contains data about the formal and informal employment, we used this to get the proportion of the occupied population that is formal and informal, nevertheless, this information is only available for 2021 and above for the Total National information, before that there is information about 23 cities and metopolitan areas. Given this issue of missing values for all the data, we will use an imputation approach for the less years.
+
+Also when we check the data about the forman and informal employment in the economic sector we noticed that the data is not reported by months but in moving average of three months. Given this we will not try to get more data about this, we will work with the proportion and the values from this data.
+
+## Preporseccing
 
 ## Models
 
-This is a work in progress
+* ARIMAX
+* Check Prophet.
--- src/epl_proyection/etl/read_informal_geih.py
+++ src/epl_proyection/etl/read_informal_geih.py
@@ -104,3 +104,98 @@ def columns_to_numeric(df:pd.DataFrame, columns: list) -> pd.DataFrame:
     for col in columns:
         df[col] = pd.to_numeric(df[col], errors='coerce')
     return df
+
+
+# GET INFORMAL DATA BY SECTOR #
+
+def read_geih_informal_data_sector(path: str, sheet_name:str) -> pd.DataFrame:
+    """
+    Reads the GEIH raw data from the specified path and return an unprocessed DataFrame.
+    Args:
+        path (str): Path to the GEIH data file.
+    Returns:
+        pd.DataFrame: Unprocessed DataFrame containing the GEIH data.
+    """
+    # Read the data
+    df = pd.read_excel(path, 
+                sheet_name = sheet_name,
+                header = 10)
+    # Filtering by neccesary columns
+    df = df.iloc[0:47].copy()
+    return df
+
+def transform_dataframe_v2_sector(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Transforms a raw DataFrame to a more usable format by performing the following steps:
+    1. Creates a transposed copy of the input DataFrame to orient the data.
+    2. Extracts the first row of the transposed DataFrame to be used as column names.
+    3. Assigns these extracted values as the new column names of the DataFrame.
+    4. Removes the original first row, which now contains the former column names.
+    5. Resets the index of the DataFrame, creating a new default integer index and a 'Year' column from the old index.
+    6. Converts the 'Year' column to a numeric data type, with any non-convertible values becoming NaN.
+    7. Fills any NaN values in the 'Year' column using the forward fill method, propagating the last valid observation forward.
+    8. Converts the 'Year' column to an integer data type.
+
+    Args:
+        df (pd.DataFrame): The raw input Pandas DataFrame.
+
+    Returns:
+        pd.DataFrame: The transformed Pandas DataFrame.
+    """
+    new_df = df.copy()
+    new_df = new_df.T
+    new_columns = new_df.iloc[0]
+    new_df.columns = new_columns
+    new_df = new_df.iloc[1:].copy()
+    new_df.reset_index(inplace=True, drop = True)
+    new_df = new_df.rename_axis(None, axis=1)
+    new_df = new_df.rename(columns = {'Concepto':'Year',np.nan:'Quarter'})
+    new_df['Year'] = pd.to_numeric(new_df['Year'], errors='coerce')
+    new_df['Year'] = new_df['Year'].ffill()
+    new_df['Year'] = new_df['Year'].astype(int)
+
+    return new_df
+
+def rename_and_filter_columns(df:pd.DataFrame, sector:list) -> pd.DataFrame:
+    """
+    Renames and filters columns in the DataFrame to keep only relevant ones.
+
+    Args:
+        df (pd.DataFrame): The input DataFrame.
+
+    Returns:
+        pd.DataFrame: The modified DataFrame with renamed and filtered columns.
+    """
+    # Loop over columns list and rename them
+    new_cols_list = []
+    cols_list = df.columns.tolist()
+    index_pob_ocupada = cols_list.index('Población ocupada')
+    index_formal = cols_list.index('Formal')
+    index_informal = cols_list.index('Informal')
+    for col in range(len(cols_list)):
+        if col <= index_formal:
+            new_cols_list.append(cols_list[col])
+        elif col > index_formal and col < index_informal:
+            new_col = 'Formal_' + cols_list[col]
+            new_cols_list.append(new_col)
+        elif col == index_informal:
+            new_cols_list.append(cols_list[col])
+        else:
+            new_col = 'Informal_' + cols_list[col]
+            new_cols_list.append(new_col)
+    
+    df.columns = new_cols_list
+
+    ## Filter by specified sectors
+    renamed_sector_informal = []
+    renamed_sector_formal = []
+    for sect in sector:
+        new_sect_formal =  'Formal_' + sect
+        new_sect_informal = 'Informal_' + sect
+        renamed_sector_formal.append(new_sect_formal)
+        renamed_sector_informal.append(new_sect_informal)
+    
+    df = df[['Year', 'Quarter', 'Población ocupada','Formal','Informal'] + sector + renamed_sector_formal + renamed_sector_informal].copy()
+
+
+    return df
